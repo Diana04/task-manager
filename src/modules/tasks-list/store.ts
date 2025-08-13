4ee64a315060aea8cf811e0f9df959ae7f4712e4
Diana Ishmatova <diana.ishmatova@gmail.com>
2025-08-13 16:48:24 +0400

    add session storage's sync for tasks store

--- src/modules/tasks-list/store.ts
+++ src/modules/tasks-list/store.ts
@@ -1,15 +1,17 @@
 import { nanoid } from 'nanoid';
 import { create } from 'zustand';
-import { persist, devtools } from 'zustand/middleware';
+import { persist, devtools, createJSONStorage } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import type { TagId, Task, TaskId, TasksFormFilters, TaskTag } from './types';
 import { TASK_TAGS_INITIAL_LIST } from './constants';
+
+const TASKS_SESSION_STORAGE_NAME = 'tasks-storage';
 
 type TasksState = {
     tasks: Task[];
     tags: TaskTag[];
     activeFilters: TasksFormFilters;
-}
+};
 
 type TasksStateActions = {
     addTask: (title: string, tagIds: TagId[]) => void;
@@ -19,39 +21,54 @@ type TasksStateActions = {
     removeTag: (tagId: TagId) => void;
 
     setActiveFilters: (filters: TasksFormFilters) => void;
-}
+};
 
 export const useTasksStore = create<TasksState & TasksStateActions>()(
-    devtools(immer(set => ({
-        tasks: [],
-        addTask: (title, tagIds) => set(state => {
-            state.tasks.push({
-                id: nanoid(),
-                title,
-                tagIds: tagIds || [],
-            });
-        }),
-        removeTask: (taskId) => set(state => {
-            state.tasks = state.tasks.filter(task => task.id !== taskId)
-        }),
+    persist(
+        devtools(
+            immer(set => ({
+                tasks: [],
+                addTask: (title, tagIds) =>
+                    set(state => {
+                        state.tasks.push({
+                            id: nanoid(),
+                            title,
+                            tagIds: tagIds || [],
+                        });
+                    }),
+                removeTask: taskId =>
+                    set(state => {
+                        state.tasks = state.tasks.filter(
+                            task => task.id !== taskId
+                        );
+                    }),
 
-        tags: TASK_TAGS_INITIAL_LIST,
-        addTag: (title, color) => set(state => {
-            state.tags.push({
-                id: nanoid(),
-                title,
-                color,
-            });
-        }),
-        removeTag: (tagId) => set(state => {
-            state.tags = state.tags.filter(tag => tag.id !== tagId)
-        }),
+                tags: TASK_TAGS_INITIAL_LIST,
+                addTag: (title, color) =>
+                    set(state => {
+                        state.tags.push({
+                            id: nanoid(),
+                            title,
+                            color,
+                        });
+                    }),
+                removeTag: tagId =>
+                    set(state => {
+                        state.tags = state.tags.filter(tag => tag.id !== tagId);
+                    }),
 
-        activeFilters: {
-            tagIds: [],
-        },
-        setActiveFilters: (filters: TasksFormFilters) => set(state => {
-            state.activeFilters = filters;
-        }),
-    })))
+                activeFilters: {
+                    tagIds: [],
+                },
+                setActiveFilters: (filters: TasksFormFilters) =>
+                    set(state => {
+                        state.activeFilters = filters;
+                    }),
+            }))
+        ),
+        {
+            name: TASKS_SESSION_STORAGE_NAME,
+            storage: createJSONStorage(() => sessionStorage),
+        }
+    )
 );
